a2c54f135a327494777a3dae0ad3df57d1952b2d
Brian McMichael <brian@brianmcmichael.com>
2017-08-14 10:21:54 -0400

    Move the fixture shapes to a separate vue objects js

---
+++ app/assets/javascripts/vue_objects.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function () {
+
+    new Vue({
+        el: "#vue-app",
+        data: {
+            fixture_shapes: [
+                { value: 'plate', title: 'Plate' },
+                { value: 't_plate', title: 'T-Plate' },
+                { value: 'pipe', title: 'Pipe'},
+                { value: 't_pipe', title: 'T-Pipe'},
+                { value: 'lap_joint', title: 'Lap Joint'}
+            ]
+
+        }
+    })
+})
