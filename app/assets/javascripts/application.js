6c04945d813a69009985f553bf0575915f532275
Brian McMichael <brian@brianmcmichael.com>
2017-08-15 09:50:32 -0400

    Import paper.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,4 +19,5 @@
 //= require three.js-r86/examples/js/controls/TrackballControls
 //= require three.js-r86/examples/js/controls/OrthographicTrackballControls
 //= require vuejs-2.4.0/vue
+//= require paper.js-0.11.4/dist/paper-full
 //= require_tree .
