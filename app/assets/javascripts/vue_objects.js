429c7af6c2013095cb5103bc8df8795556e4b9e7
Brian McMichael <brian@brianmcmichael.com>
2017-08-21 10:52:33 -0400

    Begin structuring data fror bead on plate workflow

--- app/assets/javascripts/vue_objects.js
+++ app/assets/javascripts/vue_objects.js
@@ -11,7 +11,23 @@ $(document).ready(function () {
             },
             fixture_data: {
                 fixture_shapes: [
-                    {value: 'plate', title: 'Plate'},
+                    {
+                        value: 'plate', title: 'Plate',
+                        dimensions: {
+                            width_w1: 12,
+                            width_w2: 12,
+                            thickness: 1,
+                            length: 20
+                        },
+                        joint_designs: [
+                            'bead_on_plate',
+                            'bevel_groove',
+                            'compound_bevel',
+                            'j_groove',
+                            'u_groove',
+                            'v_groove'
+                        ]
+                    },
                     {value: 't_plate', title: 'T-Plate'},
                     {value: 'pipe', title: 'Pipe'},
                     {value: 't_pipe', title: 'T-Pipe'},
@@ -21,7 +37,10 @@ $(document).ready(function () {
             },
             joint_data: {
                 joint_designs: [
-                    {value: 'bead_on_plate', title: 'Bead-on-Plate'},
+                    {
+                        value: 'bead_on_plate', title: 'Bead-on-Plate',
+                        dimensions: {}
+                    },
                     {value: 'bevel_groove', title: 'Bevel-Groove'},
                     {value: 'compound_bevel', title: 'Compund Bevel'},
                     {value: 'j_groove', title: 'J-Groove'},
@@ -30,6 +49,26 @@ $(document).ready(function () {
                     {value: 't_bevel', title: 'T-Bevel'}
                 ],
                 selected: 'bead_on_plate'
+            },
+            bead_data: {
+                bead_types: [
+                    {
+                        value: 'parabolic', title: 'Parabolic',
+                        dimensions: {
+                            x: 0.0,
+                            y: 0.35,
+                            w: 0.3,
+                            a: 0.0,
+                            b: 0.1,
+                            c: 0.0,
+                            d: -0.15,
+                            r: 0.0
+                        }
+                    },
+                    {value: 'elliptical', title: 'Elliptical'},
+                    {value: 'custom', title: 'Custom'}
+                ],
+                beads: {}
             }
         }
     })
