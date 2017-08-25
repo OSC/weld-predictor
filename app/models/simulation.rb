3dd45d67544185992e4b2f8175f0660df2de08ab
Brian McMichael <brian@brianmcmichael.com>
2017-08-25 16:38:26 -0400

    Create an input.json file on submit

--- app/models/simulation.rb
+++ app/models/simulation.rb
@@ -10,11 +10,25 @@ class Simulation < ActiveRecord::Base
     "simulation"
   end
 
+  def data
+    JSON.parse super
+  end
+
   # Define tasks to do after staging template directory typically copy over
   # uploaded files here
-  # def after_stage(staged_dir)
-  #   # CODE HERE
-  # end
+  def after_stage(staged_dir)
+    # create input.json in the root of the staged directory
+    staged_dir.join("input.json").open("w") { |f| f.write(data) }
+
+    # run substitute.py to do domain specific calculations and substitutions
+    # TODO Implement Python functionality
+    #cmd = "#{Python.exec} #{Shellwords.escape(Rails.root.join("lib", "python", "stage", "eweld_substitute.py"))} #{Shellwords.escape(staged_dir.to_s)}"
+    #stdout, stderr, status = Open3.capture3 cmd
+
+    #Rails.logger.debug "cmd: #{cmd} exited with #{status} and error: #{stderr}" unless status.exitstatus == 0
+
+    # FIXME: raise error if substitute script fails...
+  end
 
   # Build an array of Machete jobs that are then submitted to the batch server
   def build_jobs(staged_dir, job_list = [])
