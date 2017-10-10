2e81c8bfda2ac06d81cd9261dfc23fe8a5d35a1f
Brian McMichael <brian@brianmcmichael.com>
2017-10-10 13:53:07 -0400

    Serialize stl files

--- app/models/mesh.rb
+++ app/models/mesh.rb
@@ -3,8 +3,8 @@ class Mesh < ActiveRecord::Base
   validates_presence_of :inputfile
   do_not_validate_attachment_file_type :inputfile
 
-  def stl_files
-    YAML.load(super)
-  end
+  serialize :stl_files
+
+
 end
 
